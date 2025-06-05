f79bc1a67fe61efb6334dd5ea5e085c0a081f113
nsakwa <nsakwa@gmail.com>
2025-06-05 21:19:06 +0300

    skipcard.module.css update card over hue & rgba colors selectbutondisabled opacity

---
+++ src/components/SkipGrid/SkipGrid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SkipCard from '../SkipCard/SkipCard';
+import styles from './SkipGrid.module.css';
+
+const SkipGrid=({skips, selectedSkipId, onSelectSkip})=>{
+    return(
+        <div className={styles.gridContainer}>
+            {skips.map((skip)=>(
+                <SkipCard
+                    key={skip.id}
+                    skipData={skip}
+                    isSelected={skip.id === selectedSkipId}
+                    onSelectedSkip={onSelectSkip}
+            />
+            ))}
+        </div>
+    );
+};
+
+export default SkipGrid;
